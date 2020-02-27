3e6cd8c9db804e99442e92ec2ac04b6e5abce065
clemalex53 <ca.clemalexandre@gmail.com>
2020-02-27 11:11:49 +0100

    I.2. RecyclerView instantiation

--- app/src/main/java/com/example/chucknorris/MainActivity.kt
+++ app/src/main/java/com/example/chucknorris/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.chucknorris
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +12,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Display the list of jokes in the Logcat
         Log.d("list", JokeList.jokes.toString())
+        //Manager of RecyclerView
+        my_recycler_view.layoutManager = LinearLayoutManager(this)
+
     }
 }
