6af1f4ebc0d0a74856b4f89d89004505fa64ae40
clemalex53 <ca.clemalexandre@gmail.com>
2020-04-02 09:40:20 +0200

    II.5. Leaks killer

--- app/src/main/java/com/example/chucknorris/MainActivity.kt
+++ app/src/main/java/com/example/chucknorris/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.chucknorris
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import io.reactivex.Single
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,9 +22,9 @@ class MainActivity : AppCompatActivity() {
         my_recycler_view.layoutManager = LinearLayoutManager(this)
 
         val adapter = JokeAdapter()
-
         my_recycler_view.adapter = adapter
 
+        val compositeDisposable = CompositeDisposable()
         val singleJoke =
             JokeApiServiceFactory
                 .createService()
@@ -33,6 +34,8 @@ class MainActivity : AppCompatActivity() {
                     onError = {Log.e("JOKE", "error found", it)},
                     onSuccess = {Log.d("joke",it.toString())}
                 )
+        compositeDisposable.add(singleJoke);
         Log.d("joke",singleJoke.toString())
+        compositeDisposable.clear();
     }
 }
