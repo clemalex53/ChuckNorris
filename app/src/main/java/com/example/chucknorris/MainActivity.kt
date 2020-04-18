afd1b3b17f332c4a001669f9eb7a10f2614ea766
clemalex53 <ca.clemalexandre@gmail.com>
2020-04-18 18:23:46 +0200

    III.

--- app/src/main/java/com/example/chucknorris/MainActivity.kt
+++ app/src/main/java/com/example/chucknorris/MainActivity.kt
@@ -3,16 +3,16 @@ package com.example.chucknorris
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
 import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,8 +20,6 @@ class MainActivity : AppCompatActivity() {
     private val compositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -30,17 +28,13 @@ class MainActivity : AppCompatActivity() {
         val adapter = JokeAdapter()
         my_recycler_view.adapter = adapter
 
-        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
-        val button = findViewById<Button>(R.id.load_button)
-        button.setOnClickListener()
-        {
-
+        fun newJokes(n:Long){
             progressBar.visibility = View.VISIBLE
             val singleJoke =
                 JokeApiServiceFactory
                     .createService()
                     .giveMeAJoke()
-                    .repeat(10)
+                    .repeat(n)
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribeOn(Schedulers.io())
                     .subscribeBy(
@@ -55,10 +49,19 @@ class MainActivity : AppCompatActivity() {
                             progressBar.visibility = View.INVISIBLE
                         }
                     )
-
-
             compositeDisposable.add(singleJoke);
         }
+
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+
+        my_recycler_view.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (!recyclerView.canScrollVertically(1)) {
+                    newJokes(10);
+                }
+            }
+        })
     }
 
     override fun onStop() {
