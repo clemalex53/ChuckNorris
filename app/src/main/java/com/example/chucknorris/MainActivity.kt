2cd7f132e404a45113393dd684a42274af6f7eda
clemalex53 <ca.clemalexandre@gmail.com>
2020-04-18 12:49:02 +0200

    III.2. Add a loader

--- app/src/main/java/com/example/chucknorris/MainActivity.kt
+++ app/src/main/java/com/example/chucknorris/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.chucknorris
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -10,6 +12,7 @@ import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
@@ -30,22 +33,30 @@ class MainActivity : AppCompatActivity() {
         val adapter = JokeAdapter()
         my_recycler_view.adapter = adapter
 
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
         val button = findViewById<Button>(R.id.load_button)
         button.setOnClickListener()
         {
+
+            progressBar.visibility = View.VISIBLE
             val singleJoke =
                 JokeApiServiceFactory
                     .createService()
                     .giveMeAJoke()
+                    .delay(2,TimeUnit.SECONDS)
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribeOn(Schedulers.io())
                     .subscribeBy(
-                        onError = { Log.e("JOKE", "error found", it) },
+                        onError = {
+                            Log.e("JOKE", "error found", it);
+                            progressBar.visibility = View.INVISIBLE
+                        },
                         onSuccess = {
                             adapter.listOfJokes = adapter.listOfJokes.plus(it);
-                            Log.d("list", JokeList.jokesString.toString())
+                            progressBar.visibility = View.INVISIBLE
                         }
                     )
+
 
             compositeDisposable.add(singleJoke);
         }
