c032244ce92d50f2b11a0fef7dd98a3b335b99ed
clemalex53 <ca.clemalexandre@gmail.com>
2020-04-19 10:00:07 +0200

    IV.1. Manage screen rotation

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# ChuckNorris
+# Chuck Norris Joke App
+
+<p align="center" href="https://api.chucknorris.io/">
+    <img src="https://api.chucknorris.io/img/chucknorris_logo_coloured_small@2x.png" height="200">
+<p/>
+
+## Main goal
+The purpose of this application is to collect jokes about Chuck Norris on an API and display them in the form of a list of jokes that can be scrolled and which will load new ones when we have read everything.
+
+## Technical points addressed
+* dynamic list
+* json data struct
+* public web api call
+* asynchronous code
+* activity instance state
+* data share with other apps
+
+## App development
+In the "commits" window you can see the progressive development of the application in several stages:
+I. A first version of the app which displays a pre-saved list
+II. Modification of the app to get jokes from Chuck Norris web API.
+III. Display jokes from web API and automatic loading when the bottom of the list is reached
+IV. Improvement of the user interface and add of some UI features.
--- app/src/main/java/com/example/chucknorris/MainActivity.kt
+++ app/src/main/java/com/example/chucknorris/MainActivity.kt
@@ -1,46 +1,50 @@
 package com.example.chucknorris
 
+import android.R.string
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
 import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.concurrent.TimeUnit
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.builtins.list
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.parse
+import kotlinx.serialization.serializer
+import org.xmlpull.v1.XmlSerializer
+import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
 
     private val compositeDisposable = CompositeDisposable()
+    private val adapter = JokeAdapter()
+    private val serializer: KSerializer<List<Joke>> = Joke.serializer().list
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         my_recycler_view.layoutManager = LinearLayoutManager(this)
-
-        val adapter = JokeAdapter()
         my_recycler_view.adapter = adapter
 
         val progressBar = findViewById<ProgressBar>(R.id.progressBar)
-        val button = findViewById<Button>(R.id.load_button)
-        button.setOnClickListener()
-        {
 
+        fun newJokes(n:Long){
             progressBar.visibility = View.VISIBLE
             val singleJoke =
                 JokeApiServiceFactory
                     .createService()
                     .giveMeAJoke()
-                    .repeat(10)
+                    .repeat(n)
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribeOn(Schedulers.io())
                     .subscribeBy(
@@ -55,10 +59,35 @@ class MainActivity : AppCompatActivity() {
                             progressBar.visibility = View.INVISIBLE
                         }
                     )
-
-
             compositeDisposable.add(singleJoke);
         }
+
+        if(savedInstanceState != null){
+            adapter.listOfJokes =
+                Json.parse(
+                    serializer,
+                    savedInstanceState.getString("keyListOfJokes").toString()
+                )
+        }else{
+            newJokes(10)
+        }
+
+        my_recycler_view.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (!recyclerView.canScrollVertically(1)) {
+                    newJokes(3);
+                }
+            }
+        })
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        savedInstanceState.putString(
+            "keyListOfJokes",
+            Json.stringify(serializer,adapter.listOfJokes)
+        )
     }
 
     override fun onStop() {
